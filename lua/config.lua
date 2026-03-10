71496f2aa9f757d5a665b6b75d00ec065844e436
bogdan.pilkevich <trueouter@live.com>
2026-03-10 08:22:01 +0200

    Add LSP support: blazor - minor improvements in LSP integration

--- lua/config.lua
+++ lua/config.lua
@@ -1,66 +1,76 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "catppuccin" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
-
 vim.diagnostic.config({
-  underline = true,
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  severity_sort = true,
+	underline = true,
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
 -- Register Razor filetypes early so roslyn.nvim's ft trigger works
 vim.filetype.add({
-  extension = {
-    razor = 'razor',
-    cshtml = 'razor',
-  },
+	extension = {
+		razor = "razor",
+		cshtml = "razor",
+	},
 })
 
 -- Open neo-tree automatically when nvim is launched with a directory
-vim.api.nvim_create_autocmd('VimEnter', {
-  once = true,
-  callback = function()
-    local arg = vim.fn.argv(0)
-    if arg and arg ~= '' and vim.fn.isdirectory(arg) == 1 then
-      vim.cmd('Neotree show dir=' .. vim.fn.fnameescape(vim.fn.fnamemodify(arg, ':p')))
-    end
-  end,
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		local arg = vim.fn.argv(0)
+		if arg and arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+			vim.cmd("Neotree show dir=" .. vim.fn.fnameescape(vim.fn.fnamemodify(arg, ":p")))
+		end
+	end,
 })
 
 -- terminal keymaps
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('n', '<leader>rr', ':split | resize 15 | terminal cabal v2-repl<CR>i', { desc = 'Run cabal REPL in split' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set(
+	"n",
+	"<leader>rr",
+	":split | resize 15 | terminal cabal v2-repl<CR>i",
+	{ desc = "Run cabal REPL in split" }
+)
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<C-Right>", "<C-w>>", { desc = "Increase width" })
+vim.keymap.set("n", "<C-Left>", "<C-w>+", { desc = "Increase height" })
+--vim.keymap.set("n", "<C-Left>", "<C-w><", { desc = "Decrease width" })
+--vim.keymap.set("n", "<C-Up>", "<C-w>+", { desc = "Increase height" })
+--vim.keymap.set("n", "<C-Down>", "<C-w>-", { desc = "Decrease height" })
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,102 +1,126 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require('mason').setup({
-        registries = {
-          'github:mason-org/mason-registry',
-          'github:Crashdummyy/mason-registry', -- needed for roslyn
-        },
-      })
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "lua_ls", "hls", "html", "cssls", "ts_ls" },
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      { "neovim/nvim-lspconfig" },
-      { "williamboman/mason-lspconfig.nvim" },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { 'hrsh7th/cmp-nvim-lsp' },
-    config = function()
-      -- advertise nvim-cmp capabilities to LSP servers
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup({
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry", -- needed for roslyn
+				},
+			})
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "hls", "html", "cssls", "ts_ls" },
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			-- advertise nvim-cmp capabilities to LSP servers
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.lsp.config['hls'] = {
-        cmd = { 'haskell-language-server-wrapper', 'lsp' },
-        filetypes = { 'haskell', 'lhaskell', 'cabal' },
-        root_markers = { '*.cabal', 'cabal.project', 'stack.yaml', 'hie.yaml', '.git' },
-        capabilities = capabilities,
-      }
+			vim.lsp.config["hls"] = {
+				cmd = { "haskell-language-server-wrapper", "lsp" },
+				filetypes = { "haskell", "lhaskell" },
+				root_markers = { "*.cabal", "cabal.project", "stack.yaml", "hie.yaml", ".git" },
+				capabilities = capabilities,
+			}
 
-      vim.lsp.config['lua_ls'] = {
-        cmd = { 'lua-language-server' },
-        filetypes = { 'lua' },
-        root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = { version = 'LuaJIT' }
-          }
-        }
-      }
+			vim.lsp.config["lua_ls"] = {
+				cmd = { "lua-language-server" },
+				filetypes = { "lua" },
+				root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+					},
+				},
+			}
 
-      vim.lsp.enable 'lua_ls'
-      vim.lsp.enable 'hls'
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("hls")
 
-      -- LSP action keymaps (non-Telescope) — attached per buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('lsp-keymaps', { clear = true }),
-        callback = function(event)
-          local buf = event.buf
-          vim.keymap.set('n', 'grn', vim.lsp.buf.rename,       { buffer = buf, desc = '[R]e[n]ame' })
-          vim.keymap.set({ 'n', 'v' }, 'gra', vim.lsp.buf.code_action, { buffer = buf, desc = 'Code [A]ction' })
-          vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1 })  end, { buffer = buf, desc = 'Next [D]iagnostic' })
-          vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1 }) end, { buffer = buf, desc = 'Prev [D]iagnostic' })
-          vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,                   { buffer = buf, desc = 'Diagnostic [Q]uickfix list' })
-          vim.keymap.set('n', 'gl',        vim.diagnostic.open_float,                    { buffer = buf, desc = 'Diagnostic float' })
-          vim.keymap.set('n', '<leader>d', '<cmd>Telescope diagnostics bufnr=0<cr>',     { buffer = buf, desc = 'Buffer [D]iagnostics' })
-          vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics<cr>',             { buffer = buf, desc = 'Workspace [D]iagnostics' })
-        end,
-      })
-    end
-  },
-  {
-    "seblyng/roslyn.nvim",
-    ft = { "cs", "razor" },
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-    },
-    init = function()
-      -- Register razor/cshtml filetypes before the plugin loads
-      vim.filetype.add({
-        extension = {
-          razor = "razor",
-          cshtml = "razor",
-        },
-      })
+			-- LSP action keymaps (non-Telescope) — attached per buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-keymaps", { clear = true }),
+				callback = function(event)
+					local buf = event.buf
+					vim.keymap.set("n", "grn", vim.lsp.buf.rename, { buffer = buf, desc = "[R]e[n]ame" })
+					vim.keymap.set(
+						{ "n", "v" },
+						"gra",
+						vim.lsp.buf.code_action,
+						{ buffer = buf, desc = "Code [A]ction" }
+					)
+					vim.keymap.set("n", "]d", function()
+						vim.diagnostic.jump({ count = 1 })
+					end, { buffer = buf, desc = "Next [D]iagnostic" })
+					vim.keymap.set("n", "[d", function()
+						vim.diagnostic.jump({ count = -1 })
+					end, { buffer = buf, desc = "Prev [D]iagnostic" })
+					vim.keymap.set(
+						"n",
+						"<leader>q",
+						vim.diagnostic.setloclist,
+						{ buffer = buf, desc = "Diagnostic [Q]uickfix list" }
+					)
+					vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = buf, desc = "Diagnostic float" })
+					vim.keymap.set(
+						"n",
+						"<leader>d",
+						"<cmd>Telescope diagnostics bufnr=0<cr>",
+						{ buffer = buf, desc = "Buffer [D]iagnostics" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>D",
+						"<cmd>Telescope diagnostics<cr>",
+						{ buffer = buf, desc = "Workspace [D]iagnostics" }
+					)
+				end,
+			})
+		end,
+	},
+	{
+		"seblyng/roslyn.nvim",
+		ft = { "cs", "razor" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		init = function()
+			-- Register razor/cshtml filetypes before the plugin loads
+			vim.filetype.add({
+				extension = {
+					razor = "razor",
+					cshtml = "razor",
+				},
+			})
 
-      -- Configure Roslyn LSP settings
-      vim.lsp.config("roslyn", {
-        capabilities = require('cmp_nvim_lsp').default_capabilities(),
-        settings = {
-          ['csharp|background_analysis'] = {
-            dotnet_analyzer_diagnostics_scope = 'fullSolution',
-          },
-          ['csharp|code_lens'] = {
-            dotnet_enable_references_code_lens = true,
-          },
-        },
-      })
-    end,
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
-    opts = {},
-  },
+			-- Configure Roslyn LSP settings
+			vim.lsp.config("roslyn", {
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				settings = {
+					["csharp|background_analysis"] = {
+						dotnet_analyzer_diagnostics_scope = "fullSolution",
+					},
+					["csharp|code_lens"] = {
+						dotnet_enable_references_code_lens = true,
+					},
+				},
+			})
+		end,
+		---@module 'roslyn.config'
+		---@type RoslynNvimConfig
+		opts = {},
+	},
 }
